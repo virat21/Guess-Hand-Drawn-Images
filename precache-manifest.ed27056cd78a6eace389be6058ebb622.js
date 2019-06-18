84f3b12a3b680f6906170c12cc882f3c2c461b10
Virat Patel <21.virat@gmail.com>
2019-06-19 01:37:10 +0530

    Updates

---
+++ precache-manifest.ed27056cd78a6eace389be6058ebb622.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09820c525ecbb40c3f569b0739ec941f",
+    "url": "/Guess-Hand-Drawn-Images/index.html"
+  },
+  {
+    "revision": "90af11dc2b8b8dae6bdf",
+    "url": "/Guess-Hand-Drawn-Images/static/css/main.7b69cd8b.chunk.css"
+  },
+  {
+    "revision": "6bf2a1b1b425b2948a45",
+    "url": "/Guess-Hand-Drawn-Images/static/js/2.776a3fbd.chunk.js"
+  },
+  {
+    "revision": "90af11dc2b8b8dae6bdf",
+    "url": "/Guess-Hand-Drawn-Images/static/js/main.4180b989.chunk.js"
+  },
+  {
+    "revision": "7705f74cfca965dd5ef2",
+    "url": "/Guess-Hand-Drawn-Images/static/js/runtime~main.9eb31a95.js"
+  }
+]);
